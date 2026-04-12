bc161cdc1804b5d3192b45087b4df3ff923e49d0
xiejinlong <303182519@qq.com>
2026-04-12 17:11:46 +0800

    feat: 样式处理

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginSass } from '@rsbuild/plugin-sass';
+import { pluginTailwindCSS } from 'rsbuild-plugin-tailwindcss';
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
-  plugins: [pluginReact()],
+  plugins: [
+    pluginReact(),
+    pluginTailwindCSS(),
+    pluginSass()
+  ],
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import './App.css';
+import './App.scss';
 
 const App = () => {
   return (
     <div className="content">
-      <h1>Rsbuild with React</h1>
+      <h1 className="flex">Rsbuild with React</h1>
+      <h2 className="text-3xl font-bold underline">Hello world!</h2>
       <p>Start building amazing things with Rsbuild.</p>
     </div>
   );
